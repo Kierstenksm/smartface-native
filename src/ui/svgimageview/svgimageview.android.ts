e15b31e0145cf9057d93937702bac7727aed3861
Halit Aksoy <jhalitaksoy@gmail.com>
2023-04-27 16:57:37 +0300

    [FW-1398]: add svgImage property

--- src/ui/svgimageview/svgimageview.android.ts
+++ src/ui/svgimageview/svgimageview.android.ts
@@ -1,9 +1,12 @@
 import { ISvgImageView } from './svgimageview';
 import { SvgImageViewEvents } from './svgimageview-events';
 import ImageViewAndroid from '../imageview/imageview.android';
+import SvgImageAndroid from '../svgimage/svgimage.android';
 
 export default class SvgImageViewAndroid<TEvent extends string = SvgImageViewEvents> extends ImageViewAndroid<TEvent | SvgImageViewEvents> implements ISvgImageView {
   constructor(params: Partial<ISvgImageView> = {}) {
     super(params);
   }
+  get svgImage(): SvgImageAndroid {  }
+  set svgImage(value: SvgImageAndroid) {  }
 }
--- src/ui/svgimageview/svgimageview.ios.ts
+++ src/ui/svgimageview/svgimageview.ios.ts
@@ -1,9 +1,12 @@
 import { SvgImageViewEvents } from './svgimageview-events';
 import ImageViewIOS from '../imageview/imageview.ios';
 import { ISvgImageView } from './svgimageview';
+import SvgImageIOS from '../svgimage/svgimage.ios';
 
 export default class SvgImageViewIOS<TEvent extends string = SvgImageViewEvents> extends ImageViewIOS<TEvent | SvgImageViewEvents> implements ISvgImageView {
   constructor(params?: ISvgImageView) {
     super(params);
   }
+  get svgImage(): SvgImageIOS {  }
+  set svgImage(value: SvgImageIOS) {  }
 }
