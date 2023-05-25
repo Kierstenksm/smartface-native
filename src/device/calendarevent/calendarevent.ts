386eabe64cd11a00276fbbe9203eb8409119aa53
boztopuz <boztopuz@gmail.com>
2023-05-25 14:33:03 +0300

    [Fw-1512]: added new module for add event for Calender

--- src/device/calendarevent/calendarevent.ts
+++ src/device/calendarevent/calendarevent.ts
@@ -19,10 +19,10 @@ export type PresentEventCreatingDialogParams = {
  *     CalendarEvent.presentEventCreatingDialog({
  *       title: "Event Title",
  *       description: "Event Description",
- *       location: "43.5565, 37.43434",
- *       startDate: "2023-06-23T10:45",
- *       endDate: "2023-06-23T14:45",
- *       allDay: false
+ *       location: "Ankara, Etimesgut, Türkiye",
+ *       startDate: "2023-05-26T07:10:42.616+03:00",
+ *       endDate: "2023-05-26T13:30:42.616+03:00",
+ *       allDay: false,
  *     });
  */
 export interface ICalendarEvent {
@@ -30,14 +30,26 @@ export interface ICalendarEvent {
 
 export abstract class AbstractCalendarEvent implements ICalendarEvent {
   /**
-   * Use presentEventCreatingDialog to open a calendar for the add calendar event.
+   * Use presentEventCreatingDialog to open a calendar for the add calendar event. {@link Permissions#calendar} Permisson is required for iOS to access native Calender app.
    *
+   * @example
+   *     import CalendarEvent from '@smartface/native/device/calendarevent';
+   *
+   *     CalendarEvent.presentEventCreatingDialog({
+   *       title: "Event Title",
+   *       description: "Event Description",
+   *       location: "Ankara, Etimesgut, Türkiye",
+   *       startDate: "2023-05-26T07:10:42.616+03:00",
+   *       endDate: "2023-05-26T13:30:42.616+03:00",
+   *       allDay: false,
+   *     });
+   * 
    * @method presentEventCreatingDialog
    * @param {String} params.title title of event
    * @param {String} params.description notes (iOS) or description (Android) associated with the event.
    * @param {String} params.location location of event
-   * @param {String} params.startDate start date of event in UTC, format: 'YYYY-MM-DDTHH:mm:ss.SSSZ'
-   * @param {String} params.endDate end date of event in UTC, format: 'YYYY-MM-DDTHH:mm:ss.SSSZ'
+   * @param {String} params.startDate start date of event in UTC, format: 'yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ'
+   * @param {String} params.endDate end date of event in UTC, format: 'yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ'
    * @param {Boolean} params.allDay event cover the whole day or not
    * @static
    * @android
