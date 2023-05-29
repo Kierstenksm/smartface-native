0905cadeafa308b57d93f22f57dcaade4a436114
boztopuz <boztopuz@gmail.com>
2023-05-29 12:36:08 +0300

    [Fw-1512]: change creating instance situation. One instance is being created now in every time

--- src/device/calendarevent/calendarevent.ios.ts
+++ src/device/calendarevent/calendarevent.ios.ts
@@ -1,9 +1,10 @@
 import { AbstractCalendarEvent, PresentEventCreatingDialogParams } from './calendarevent';
 
 class CalendarEventIOS implements AbstractCalendarEvent {
+  static calenderEvent = new __SF_CalenderEvent()
+
   static presentEventCreatingDialog(params: PresentEventCreatingDialogParams) {
-    const calenderEvent = new __SF_CalenderEvent()
-    calenderEvent.presentEventCreatingDialog(params.title, params.description, params.location, params.startDate, params.endDate, params.allDay)
+    CalendarEventIOS.calenderEvent.presentEventCreatingDialog(params.title, params.description, params.location, params.startDate, params.endDate, params.allDay)
   }
 }
 
